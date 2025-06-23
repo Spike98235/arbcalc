b74e64c4803e2c5ff23c64eaa7ed4b373b8ddd36
Spike98235 <shivangyadav9929@gmail.com>
2025-06-23 07:28:23 +0530

    Initial upload

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+// main.dart (shortened for brevity)
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(const ArbitrageApp());
+
+// ... (rest of code already provided in prior steps)
